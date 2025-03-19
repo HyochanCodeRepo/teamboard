20f5f26b54cb0401de36f00e467f1c82d4dfcc94
harry <hyochan.lee91@gmail.com>
2025-03-19 10:15:28 +0900

    init

---
+++ src/main/java/com/example/teamboard/TeamboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamboardApplication.class, args);
+    }
+
+}
